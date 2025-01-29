71488d41f2a8f2d95f2a69ac3dcac15b8b3c392d
drockparashar <pranshu007parashar@gmail.com>
2025-01-29 19:09:11 +0530

    Updated UI

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { Lock, Mail, User, ArrowRight, CheckCircle } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
